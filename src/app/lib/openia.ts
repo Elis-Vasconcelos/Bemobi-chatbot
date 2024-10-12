12078a0da89a7743c6a888c7d8f28f0b4a45565e
Elis-Vasconcelos <elisvasconcelos@infojr.com.br>
2024-10-12 16:31:21 -0300

    fix: stop showing .env

--- src/app/lib/openia.ts
+++ src/app/lib/openia.ts
@@ -1,14 +1,15 @@
 import 'dotenv/config';
 import OpenAI from 'openai';
 
+const API_KEY: string = process.env.OPENAI_API_KEY
+
 const openai = new OpenAI({
-  apiKey: "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",
-  dangerouslyAllowBrowser: true
+  apiKey: API_KEY
 });
 
 export const generateAIResponse = async (prompt: string) => {
     const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo', // ou outro modelo como 'gpt-3.5-turbo'
+      model: 'gpt-4o-mini',
       messages: [{ role: "user", content: prompt }],
       temperature: 0.7,
     });
@@ -19,10 +20,3 @@ export const generateAIResponse = async (prompt: string) => {
       throw new Error("Resposta inválida da API");
     }
   };
-  
-  async function main() {
-    const result = await generateAIResponse("Diga que isso é um teste!");
-    console.log(result);
-  }
-  
-  main();
